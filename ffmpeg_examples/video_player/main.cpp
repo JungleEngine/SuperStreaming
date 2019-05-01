11af5788d888506b3af42c8c91054aaebee4a6a0
Dawod <mhm.dawod@gmail.com>
2019-05-01 10:05:41 +0200

    implementing (pause) function in the player

--- ffmpeg_examples/video_player/main.cpp
+++ ffmpeg_examples/video_player/main.cpp
@@ -31,6 +31,9 @@ extern "C"{
 #define av_err2str(errnum) av_make_error_string((char*)__builtin_alloca(AV_ERROR_MAX_STRING_SIZE), AV_ERROR_MAX_STRING_SIZE, errnum)
 #define av_ts2str(ts) av_ts_make_string((char*)__builtin_alloca(AV_TS_MAX_STRING_SIZE), ts)
 
+int is_paused = 0;
+int last_paused = is_paused;
+
 int received_frames = 0;
 int written_packets_count = 0;
 int largest_packet_size = 0;
@@ -92,6 +95,44 @@ void packet_queue_init(PacketQueue *q) {
     q->cond = SDL_CreateCond();
 }
 
+
+//BUG : this function in still buggy- some times the progrom doesn't close safely
+void do_quit(AVFrame* frame){
+
+//the following lines (and more) to close the stream {stream_close()}
+    av_frame_free(&frame);
+    swr_free(&swr);
+
+    for (int i = 0; i < ifmt_ctx->nb_streams; i++) {
+        avcodec_free_context(&stream_ctx[i].dec_ctx);
+        if (ofmt_ctx && ofmt_ctx->nb_streams > i && ofmt_ctx->streams[i] && stream_ctx[i].enc_ctx)
+            avcodec_free_context(&stream_ctx[i].enc_ctx);
+    }    
+    avformat_close_input(&ifmt_ctx);
+    SDL_DestroyTexture(texture);
+    av_free(stream_ctx);
+
+    //--------------------------------------------------------
+    //you may need to check first if there is render object!
+    SDL_DestroyRenderer(renderer);
+    //you may need to check first if there is a window object !
+    SDL_DestroyWindow(window);
+
+#if CONFIG_AVFILTER
+    av_freep(&vfilters_list);//check the usage of this line and differents between ( av_Free() & av_Freep() )
+#endif
+
+/*
+ *  Undo the initialization done by avformat_network_init. Call it only
+ *  once for each time you called avformat_network_init.
+ */
+    avformat_network_deinit();//check the usage of this line
+    SDL_Quit();
+    av_log(NULL, AV_LOG_QUIET, "%s", ""); //we may change this line
+    exit(0);
+}
+
+
 int packet_queue_put(PacketQueue *q, AVPacket *pkt) {
     AVPacketList *pkt1;
     // init packet
@@ -119,6 +160,30 @@ int packet_queue_put(PacketQueue *q, AVPacket *pkt) {
     SDL_UnlockMutex(q->mutex);
     return 0;
 }
+
+
+
+/* 
+static void packet_queue_flush(PacketQueue *q)
+{
+    MyAVPacketList *pkt, *pkt1;
+
+    SDL_LockMutex(q->mutex);
+    for (pkt = q->first_pkt; pkt; pkt = pkt1) {
+        pkt1 = pkt->next;
+        av_packet_unref(&pkt->pkt);
+        av_freep(&pkt);
+    }
+    q->last_pkt = NULL;
+    q->first_pkt = NULL;
+    q->nb_packets = 0;
+    q->size = 0;
+    q->duration = 0;
+    SDL_UnlockMutex(q->mutex);
+}
+*/
+
+
 
 // Pull packet from queue.
 // May be blocket until packet received or just continue without blocking.
@@ -241,43 +306,41 @@ int audio_decode_frame(AVCodecContext * acodec_ctx, uint8_t *audio_buf, int buf_
             quit = 1; // stop program
             return -1;
         }
-        ret = avcodec_send_packet(acodec_ctx, &pkt);
-        if (ret < 0) {
-            printf("decoding failed: %s \n", av_err2str(ret));
-            /* if error, skip frame */
-            return -1;
-        } else {
-            while (ret >= 0) {
-                frame = av_frame_alloc();
-                ret = avcodec_receive_frame(acodec_ctx, frame);
-                if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF) {
-                    printf("Decoding audio: %s \n", av_err2str(ret));
-                    break;
-                } else {
-                    reframe = av_frame_alloc();
-                    reframe->channel_layout = frame->channel_layout;
-                    reframe->sample_rate = frame->sample_rate;
-                    reframe->format = OUT_SAMPLE_FMT;
-                    int swr_state = swr_convert_frame(swr, reframe, frame);
-                    if (swr_state < 0) {
-                        // convert error, try next frame
-                        printf("Audio convert error, try next frame\n");
-                        continue;
-                    }
-                    data_size += av_samples_get_buffer_size(NULL, reframe->channels,
-                                                           reframe->nb_samples,
-                                                           static_cast<AVSampleFormat>(reframe->format), 0);
-                    memcpy(audio_buf, reframe->data[0], static_cast<size_t>(data_size));
-                    av_frame_free(&reframe);
-                    av_frame_free(&frame);
-
-
-
-                }
-
-            }
-
-        }
+	if(!is_paused)
+	{
+		ret = avcodec_send_packet(acodec_ctx, &pkt);
+		if (ret < 0) {
+			printf("decoding failed: %s \n", av_err2str(ret));
+			/* if error, skip frame */
+			return -1;
+		} else {
+			while (ret >= 0) {
+				frame = av_frame_alloc();
+                		ret = avcodec_receive_frame(acodec_ctx, frame);
+                		if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF) {
+                    		printf("Decoding audio: %s \n", av_err2str(ret));
+                    		break;
+				} else {
+					reframe = av_frame_alloc();
+                    			reframe->channel_layout = frame->channel_layout;
+					reframe->sample_rate = frame->sample_rate;
+					reframe->format = OUT_SAMPLE_FMT;
+					int swr_state = swr_convert_frame(swr, reframe, frame);
+					if (swr_state < 0) {
+						// convert error, try next frame
+						printf("Audio convert error, try next frame\n");
+						continue;
+					}
+					data_size += av_samples_get_buffer_size(NULL, reframe->channels,
+							reframe->nb_samples,
+							static_cast<AVSampleFormat>(reframe->format), 0);
+					memcpy(audio_buf, reframe->data[0], static_cast<size_t>(data_size));
+					av_frame_free(&reframe);
+					av_frame_free(&frame);
+				}
+			}
+		}
+	}
 
         if(pkt.data) {// not null
             av_packet_unref(&pkt);
@@ -432,7 +495,49 @@ static int init_sdl(){
 
 }
 
+void event_handler(SDL_Event event, AVFrame* frame){
+	
+        switch (event.type) {
+            case SDL_QUIT:
+                printf("SDL receive stop, try shutdown\n");
+                quit = 1;
+                break;
+	    case SDL_KEYDOWN:
+		if (event.key.keysym.sym == SDLK_ESCAPE || event.key.keysym.sym == SDLK_q) {
+			printf("DAWOD :*****************************************************************QUIT\n");
+			do_quit(frame);
+			break;
+		}
+		switch (event.key.keysym.sym) {
+			case SDLK_SPACE:
+			case SDLK_p:
+				if (!is_paused){
+					printf("DAWOD :************************************************************* PAUSE\n");
+				}
+				else{
+					printf("DAWOD :************************************************************* RESUME\n");
+				}
+				is_paused = is_paused ^ 1;
 
+				break;
+			case SDLK_m:
+				printf("DAWOD :************************************************************** MUTE\n");
+				break;
+			case SDLK_LEFT:
+//				incr = seek_interval ? -seek_interval : -10.0;
+				printf("DAWOD :************************************************************** SEEK BACK\n");
+				break;
+			case SDLK_RIGHT:
+//				incr = seek_interval ? seek_interval : 10.0;
+				printf("DAWOD :************************************************************** SEEK FORWARD\n");
+				break;
+			default:
+				break;
+		}
+            default:
+                break;
+        }
+}
 
 
 
@@ -448,7 +553,8 @@ int main(int argc, char **argv) {
     int stream_index;
     enum AVMediaType type;
 
-    ret = open_input_file("/home/dawod/Videos/bazringosh.mp4");
+    ret = open_input_file("rtsp://127.0.0.1:8000/gamar.mkv");
+    //ret = open_input_file("/home/dawod/Videos/bazringosh.mp4");
     if (ret < 0) {
         printf("couldn't open input file\n");
         exit(1);
@@ -481,111 +587,174 @@ int main(int argc, char **argv) {
     // Read all packets.
     while (quit != 1) {
         printf("in loop \n");
-        SDL_PollEvent(&event);
-        switch (event.type) {
-            case SDL_QUIT:
-                printf("SDL receive stop, try shutdown\n");
-                quit = 1;
-                break;
-            default:
-                break;
+	SDL_PollEvent(&event);
+	printf("dawod : 1\n");
+	SDL_PumpEvents();
+	printf("dawod : 2\n");
+
+//    while (!SDL_PeepEvents(event, 1, SDL_GETEVENT, SDL_FIRSTEVENT, SDL_LASTEVENT)) {
+//	    SDL_PumpEvents();
+//    }
+
+	event_handler(event, frame);
+	printf("dawod : 3\n");
+
+
+        if (is_paused != last_paused) {
+		last_paused = is_paused;
+		if (is_paused)
+			av_read_pause(ifmt_ctx);
+		else
+			av_read_play(ifmt_ctx);
         }
 
-        if ((ret = av_read_frame(ifmt_ctx, packet)) < 0) {
-            if(put_all!=1){
-                printf("Complete read frames\n");
-                put_all = 1;
-            }
-            sleep(1);
-            continue;
-        }
 
-        if (first_packet_size_read == 0)
-            first_packet_size_read = packet->size;
-        stream_index = packet->stream_index;
-        type = ifmt_ctx->streams[packet->stream_index]->codecpar->codec_type;
-        av_log(NULL, AV_LOG_DEBUG, "Demuxer gave frame of stream_index %u\n",
-               stream_index);
 
-        av_packet_rescale_ts(packet,
-                             ifmt_ctx->streams[stream_index]->time_base,
-                             stream_ctx[stream_index].dec_ctx->time_base);
 
-        if (type == AVMEDIA_TYPE_VIDEO) {
-            largest_input_packet_size = std::max(largest_input_packet_size, packet->size);
-            ret = avcodec_send_packet(stream_ctx[stream_index].dec_ctx, packet);
+        if (is_paused) {
+		/*  wait 10 ms to avoid trying to get another packet */
+		/*  XXX: horrible */
+		SDL_Delay(10);
+		continue;
+	}
+	
+
+/* 
+        if (is_seek_req) {
+            int64_t seek_target = is->seek_pos;
+            int64_t seek_min    = is->seek_rel > 0 ? seek_target - is->seek_rel + 2: INT64_MIN;
+            int64_t seek_max    = is->seek_rel < 0 ? seek_target - is->seek_rel - 2: INT64_MAX;
+// FIXME the +-2 is due to rounding being not done in the correct direction in generation
+//      of the seek_pos/seek_rel variables
+
+            ret = avformat_seek_file(is->ic, -1, seek_min, seek_target, seek_max, is->seek_flags);
             if (ret < 0) {
-                av_frame_free(&frame);
-                av_log(NULL, AV_LOG_ERROR, "Decoding failed\n");
-                break;
+                av_log(NULL, AV_LOG_ERROR,
+                       "%s: error while seeking\n", is->ic->url);
             } else {
-                // Decode.
-                while (ret >= 0) {
-                    frame = av_frame_alloc();
-                    ret = avcodec_receive_frame(stream_ctx[stream_index].dec_ctx, frame);
-                    if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
-                        break;
-                    else if (ret < 0) {
-                        fprintf(stderr, "Error during decoding\n");
-                        exit(1);
-                    }
-
-
-                    received_frames++;
-
-                    AVFrame pict;
-                    pict.data[0] = yPlane;
-                    pict.data[1] = uPlane;
-                    pict.data[2] = vPlane;
-                    pict.linesize[0] = stream_ctx[stream_index].dec_ctx->width;
-                    pict.linesize[1] = uvPitch;
-                    pict.linesize[2] = uvPitch;
-
-                    /* convert to destination format */
-                    sws_scale(sws_ctx, (const uint8_t *const *) frame,
-                              frame->linesize, 0, stream_ctx[stream_index].dec_ctx->height,
-                              pict.data, pict.linesize);
-
-
-                    SDL_UpdateYUVTexture(
-                            texture,
-                            NULL,
-                            yPlane,
-                            stream_ctx[stream_index].dec_ctx->width,
-                            uPlane,
-                            uvPitch,
-                            vPlane,
-                            uvPitch
-                    );
-
-                    SDL_RenderClear(renderer);
-                    SDL_RenderCopy(renderer, texture, NULL, NULL);
-                    SDL_RenderPresent(renderer);
-
-                    SDL_PollEvent(&event);
-                    switch (event.type) {
-                        case SDL_QUIT:
-                            quit = 1;
-                            SDL_Quit();
-                            exit(0);
-                            break;
-                        default:
-                            break;
-                    }
-                    printf("showing frame:%d \n", ++received_frames);
-
-                    av_frame_free(&frame);
-                    if (ret < 0) {
-                        printf("badddd\n");
-                        exit(1);
-                    }
+                if (is->audio_stream >= 0) {
+                    packet_queue_flush(&is->audioq);
+                    packet_queue_put(&is->audioq, &flush_pkt);
                 }
-
+                if (is->subtitle_stream >= 0) {
+                    packet_queue_flush(&is->subtitleq);
+                    packet_queue_put(&is->subtitleq, &flush_pkt);
+                }
+                if (is->video_stream >= 0) {
+                    packet_queue_flush(&is->videoq);
+                    packet_queue_put(&is->videoq, &flush_pkt);
+                }
+                if (is->seek_flags & AVSEEK_FLAG_BYTE) {
+                   set_clock(&is->extclk, NAN, 0);
+                } else {
+                   set_clock(&is->extclk, seek_target / (double)AV_TIME_BASE, 0);
+                }
             }
-
-        } else if (type == AVMEDIA_TYPE_AUDIO) {
-            packet_queue_put(&audioq, packet);
+            is->seek_req = 0;
+            is->queue_attachments_req = 1;
+            is->eof = 0;
+            if (is->paused)
+                step_to_next_frame(is);
         }
+*/
+
+	if(!is_paused){
+		if ((ret = av_read_frame(ifmt_ctx, packet)) < 0) {
+			if(put_all!=1){
+				printf("Complete read frames\n");
+				put_all = 1;
+			}
+			sleep(1);
+			continue;
+		}
+		
+		if (first_packet_size_read == 0)
+			first_packet_size_read = packet->size;
+		stream_index = packet->stream_index;
+		type = ifmt_ctx->streams[packet->stream_index]->codecpar->codec_type;
+		av_log(NULL, AV_LOG_DEBUG, "Demuxer gave frame of stream_index %u\n",
+				stream_index);
+		
+		av_packet_rescale_ts(packet,
+				ifmt_ctx->streams[stream_index]->time_base,
+				stream_ctx[stream_index].dec_ctx->time_base);
+	}
+	
+	
+	
+	if (type == AVMEDIA_TYPE_VIDEO) {
+		largest_input_packet_size = std::max(largest_input_packet_size, packet->size);
+
+	    
+		if(!is_paused)
+		{
+			ret = avcodec_send_packet(stream_ctx[stream_index].dec_ctx, packet);
+			if (ret < 0) {
+				av_frame_free(&frame);
+				av_log(NULL, AV_LOG_ERROR, "Decoding failed\n");
+				break;
+			} 
+			else 
+			{
+				// Decode.
+				while (ret >= 0) {
+				    frame = av_frame_alloc();
+				    ret = avcodec_receive_frame(stream_ctx[stream_index].dec_ctx, frame);
+				    if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
+					    break;
+				    else if (ret < 0) {
+					    fprintf(stderr, "Error during decoding\n");
+					    exit(1);
+				    }
+				    
+				    
+				    received_frames++;
+				    
+				    AVFrame pict;
+				    pict.data[0] = yPlane;
+				    pict.data[1] = uPlane;
+				    pict.data[2] = vPlane;
+				    pict.linesize[0] = stream_ctx[stream_index].dec_ctx->width;
+				    pict.linesize[1] = uvPitch;
+				    pict.linesize[2] = uvPitch;
+				    
+				    /* convert to destination format */
+				    sws_scale(sws_ctx, (const uint8_t *const *) frame,
+						    frame->linesize, 0, stream_ctx[stream_index].dec_ctx->height,
+						    pict.data, pict.linesize);
+				    
+				    
+				    SDL_UpdateYUVTexture(
+					    texture,
+					    NULL,
+					    yPlane,
+					    stream_ctx[stream_index].dec_ctx->width,
+					    uPlane,
+					    uvPitch,
+					    vPlane,
+					    uvPitch
+						    );
+				    
+				    SDL_RenderClear(renderer);
+				    SDL_RenderCopy(renderer, texture, NULL, NULL);
+				    SDL_RenderPresent(renderer);
+		    
+				    
+				    printf("showing frame:%d \n", ++received_frames);
+				    
+				    av_frame_free(&frame);
+				    if (ret < 0) {
+					    printf("badddd\n");
+					    exit(1);
+				    }
+			    }
+		    }
+	    }
+	
+	} 
+	else if (type == AVMEDIA_TYPE_AUDIO) {
+		packet_queue_put(&audioq, packet);
+	}
 
     }
     SDL_Quit();
